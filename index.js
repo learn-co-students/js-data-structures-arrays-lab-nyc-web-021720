acfb1c92ee082160e6f6a2c097da8b0a45e5d31a
jguzz <joseph.a.guzzardo@gmail.com>
2020-03-30 13:40:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your solution here!
+// Write your solution here
+
+const drivers = [`Milo`, `Otis`, `Garfield`];
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name);
+  return drivers;
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+  return drivers;
+}
